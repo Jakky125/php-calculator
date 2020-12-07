26dfcfd9de8822afa8b5a9fc5e7134c5706d3d62
Jakky125 <75451021+Jakky125@users.noreply.github.com>
2020-12-07 14:04:06 +0100

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -1,3 +1,8 @@
 <?php
 declare(strict_types = 1);
-echo "Hello";
+$zadani = explode("+", $argv[1]);
+$vysledek = 0;
+foreach($zadani as $hodnota){
+    $vysledek = $vysledek + $hodnota;
+}
+print_r($vysledek);
