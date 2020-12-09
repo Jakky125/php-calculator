4e0e170dcaf64470b6c5eaa2be608e6460eaeab8
Jakky125 <75451021+Jakky125@users.noreply.github.com>
2020-12-09 12:40:09 +0100

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -53,6 +53,37 @@ else
 echo $vysledek;
 
 
+<?php
+declare(strict_types = 1);
+$string = $argv[1];
+$vysledek = 0;
+function solveplusminus($string, $vysledek){
+    $plusIndex = strrpos($string, '+'); // index posledního pluska
+    $minusIndex = strrpos($string, '-'); //index posledního minus
+if(empty($plusIndex) AND empty($minusIndex)) {
+    $vysledek = $vysledek + (int)$string;
+}
+if($plusIndex > $minusIndex)
+    {
+    $delkaposlednicasti = strlen($string) - $plusIndex;
+    $firstpart = substr($string,0,$plusIndex);
+    $secondpart = substr($string,$plusIndex, $delkaposlednicasti);
+    $vysledek = $vysledek + (int)$secondpart;
+    $string = substr($string, 0,-$secondpart);
+    }
+else
+    {
+    $delkaposlednicasti = strlen($string) - $minusIndex;
+    $firstpart = substr($string,0,$minusIndex);
+    $secondpart = substr($string,$minusIndex, $delkaposlednicasti);
+    $vysledek = $vysledek - (int)$secondpart;
+    $string = substr($string, 0,-$secondpart);
+    }
+solveplusminus($string, $vysledek);
+}
+echo $vysledek;
+
+
 
 
 
