18b33d0a9813c0932c7031df89f7efaf38b4a767
Jakky125 <75451021+Jakky125@users.noreply.github.com>
2021-01-05 15:30:58 +0100

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -1,32 +1,77 @@
 <?php
 declare(strict_types = 1);
 $string = $argv[1];
-function solveplusminus($string){
-    $plusIndex = strrpos($string, '+'); // index posledního pluska
-    $minusIndex = strrpos($string, '-'); //index posledního minus
-if(empty($plusIndex) AND empty($minusIndex)) {
-    $vysledek = $vysledek + (int)$string;
-    echo $vysledek;
-}
-else{
-if($plusIndex > $minusIndex)
-    {
-    $delkaposlednicasti = strlen($string) - $plusIndex;
-    $firstpart = substr($string,0,$plusIndex);
-    $secondpart = substr($string,$plusIndex, $delkaposlednicasti);
-    $vysledek = $vysledek + (int)$secondpart;
-    $string = substr($string, 0,-$secondpart);
-    solveplusminus($string);
+
+function solveplusminus(string $string) : int {
+    $plusIndex = strrpos($string, '+');
+    $minusIndex = strrpos($string, '-');
+
+    if ($plusIndex === false && $minusIndex === false) {
+        return (int) $string;
     }
-else
-    {
-    $delkaposlednicasti = strlen($string) - $minusIndex;
-    $firstpart = substr($string,0,$minusIndex);
-    $secondpart = substr($string,$minusIndex, $delkaposlednicasti);
-    $vysledek = $vysledek - (int)$secondpart;
-    $string = substr($string, 0,-$secondpart);
-    solveplusminus($string);
+
+    if ($plusIndex > $minusIndex) {
+        $firstPart = substr($string,0, $plusIndex);
+        $secondPart = substr($string, $plusIndex + 1);
+
+        return solveplusminus($firstPart) + solveplusminus($secondPart);
     }
+
+    $firstPart = substr($string,0, $minusIndex);
+    $secondPart = substr($string, $minusIndex + 1);
+
+    return solveplusminus($firstPart) - solveplusminus($secondPart);
 }
+
+function solveMultiDivision(string $string) : int {
+    $multiIndex = strpos($string, "*");
+    $divisionIndex = strpos($string, "/");
+
+    if ($multiIndex === false && $divisionIndex === false) {
+        echo solveplusminus($string);
+    }
+
+    if ($multiIndex < $divisionIndex) {
+        $firstPart = substr($string,0, $multiIndex);
+        $lastIndexPlus = strrpos($firstPart, "+");
+        $lastIndexMinus = strrpos($firstPart, "-");
+        if ($lastIndexPlus > $lastIndexMinus)
+        {
+            $firstPart = substr($firstPart,$lastIndexPlus + 1);
+        }
+        $firstPart = substr($firstPart,$lastIndexMinus + 1);
+        $secondPart = substr($string, $multiIndex + 1);
+        $lastIndexPlus = strpos($secondPart, "+");
+        $lastIndexMinus = strpos($secondPart, "-");
+        if ($lastIndexPlus > $lastIndexMinus)
+        {
+            $secondPart = substr($secondPart,0,$lastIndexMinus);
+        }
+        $secondPart = substr($secondPart,0,$lastIndexPlus);
+        return solveMultiDivision($firstPart) * solveMultiDivision($secondPart);
+
+    }
+
+    if ($multiIndex > $divisionIndex) {
+        $firstPart = substr($string,0, $divisionIndex);
+        $lastIndexPlus = strrpos($firstPart, "+");
+        $lastIndexMinus = strrpos($firstPart, "-");
+        if ($lastIndexPlus > $lastIndexMinus)
+        {
+            $firstPart = substr($firstPart,$lastIndexPlus + 1);
+        }
+        $firstPart = substr($firstPart,$lastIndexMinus + 1);
+        $secondPart = substr($string, $divisionIndex + 1);
+        $lastIndexPlus = strpos($secondPart, "+");
+        $lastIndexMinus = strpos($secondPart, "-");
+        if ($lastIndexPlus > $lastIndexMinus)
+        {
+            $secondPart = substr($secondPart,0,$lastIndexMinus);
+        }
+        $secondPart = substr($secondPart,0,$lastIndexPlus);
+        return solveMultiDivision($firstPart) * solveMultiDivision($secondPart);
+
+    }
+
 }
-solveplusminus($string);
+echo solveMultiDivision($string);
